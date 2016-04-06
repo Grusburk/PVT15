787f0f530dc6fbd8fcfb01f9a31c2fa0e9ec339f
aysterberi <filipoppel@hotmail.com>
2016-04-06 08:12:26 +0200

    Test class

---
+++ app/src/main/java/grupp2/satansdemocracy/SatansDemokratiTest.java
@@ -0,0 +1,12 @@
+package grupp2.satansdemocracy;
+
+import org.junit.Test;
+
+/**
+ * Created by Joakim on 2016-04-06.
+ */
+public class SatansDemokratiTest {
+    /**
+     * TODO: Write tests, such as JSON format testing etc
+     */
+}
