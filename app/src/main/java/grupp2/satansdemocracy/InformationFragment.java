ca46630ae0411cea8f7601d056f050cf9e6cec27
aysterberi <filipoppel@hotmail.com>
2016-05-02 16:52:40 +0200

    Hitta hit- och köp biljett-knapparna gör det dem ska

--- app/src/main/java/grupp2/satansdemocracy/InformationFragment.java
+++ app/src/main/java/grupp2/satansdemocracy/InformationFragment.java
@@ -1,20 +1,26 @@
 package grupp2.satansdemocracy;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.*;
+import android.widget.Button;
 
 public class InformationFragment extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+    private String sdLatLng = "59.3046505, 18.1178092";
 
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+    private Button directionButton, ticketButton;
 
 
     public InformationFragment() {
@@ -46,5 +52,29 @@ public class InformationFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_information, container, false);
     }
 
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
 
+        directionButton = (Button) view.findViewById(R.id.hitta_hit_button);
+        ticketButton = (Button) view.findViewById(R.id.biljett_button);
+
+        directionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent directionIntent = new Intent(Intent.ACTION_VIEW,
+                        Uri.parse("http://maps.google.com/maps?daddr=59.3046505,18.1178092"));
+                startActivity(directionIntent);
+            }
+        });
+
+        ticketButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent ticketIntent = new Intent(Intent.ACTION_VIEW,
+                        Uri.parse("https://kulturbiljetter.se/evenemang/satans-delirium-del-2-i-satans-trilogi-2269"));
+                startActivity(ticketIntent);
+            }
+        });
+    }
 }
